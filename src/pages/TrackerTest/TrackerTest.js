79f808d33278f3066a7e429460c01611fb3a8449
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2022-11-19 14:32:30 +0800

    Add webcam component with react-webcam

--- src/pages/TrackerTest/TrackerTest.js
+++ src/pages/TrackerTest/TrackerTest.js
@@ -1,18 +1,48 @@
 import React from "react";
+import Webcam from "react-webcam";
+
 
 export default function TrackerTest({}) {
+    // play webcam video
+    // https://www.npmjs.com/package/react-webcam
+    
+    const WebcamComponent = () => <Webcam />;
+    const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+    };
+
+    const WebcamCapture = () => (
+    <Webcam
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+    >
+        {({ getScreenshot }) => (
+        <button
+            onClick={() => {
+            const imageSrc = getScreenshot()
+            }}
+        >
+            Capture photo
+        </button>
+        )}
+    </Webcam>
+    );
+
   return (
     <div>
       <h1>Tracker Test</h1>
-      <div id="main">
+      <div id="tracker-main">
         <div class="container">
-          <div class="canvas-wrapper">
-            <canvas id="output"></canvas>
-            <video
-              id="video"
-            ></video>
-          </div>
-          <div id="scatter-gl-container"></div>
+          {
+            <WebcamCapture />
+          }
+
+
         </div>
       </div>
     </div>
