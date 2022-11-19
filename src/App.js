ec4ef89b9be59ea607ef2e04934e2e1a33a9d57f
EmilyTJX <tockemily@yahoo.com.sg>
2022-11-20 06:26:07 +0800

    add in cloudchase pixi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import TrackerTest from "./pages/TrackerTest/TrackerTest";
+import CloudChase2 from "./pages/CloudChase/CloudChase2";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/tracker-test" element={<TrackerTest />} />
+          <Route path="/cloud-chase" element={<CloudChase2 />} />
           <Route path="*" element={<HomePage /> /* <NotFound /> */} />
         </Routes>
       </BrowserRouter>
